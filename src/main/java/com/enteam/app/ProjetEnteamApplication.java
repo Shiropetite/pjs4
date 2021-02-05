7b3bf72415aba175eb9f6572e97f191aac1d4f52
PaydiLuv <anykennylster@gmail.com>
2021-02-05 10:27:21 +0100

    Fist initialisation NotiFeed startin
    Created the project, added a couple stuff for the Notification feed, mostly a save while on meeting

---
+++ src/main/java/com/enteam/app/ProjetEnteamApplication.java
@@ -0,0 +1,13 @@
+package com.enteam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEnteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEnteamApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/enteam/app/serviceImpl/NotificationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.enteam.app.serviceImpl;
+
+import com.enteam.app.services.NotificationFeedService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationServiceImpl implements NotificationFeedService {
+}
