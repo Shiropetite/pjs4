57b1bc97879d49bc3479766aa6f87f5e607a06fc
PaydiLuv <anykennylster@gmail.com>
2021-02-19 10:46:17 +0100

    Added todocontroller
    todocontroller added, need JPA to make the logic behind

--- src/main/java/com/enteam/app/controllers/NotificationFeedController.java
+++ src/main/java/com/enteam/app/controllers/NotificationFeedController.java
@@ -1,18 +1,23 @@
 package com.enteam.app.controllers;
 
 import com.enteam.app.notifying.Notification;
+import com.enteam.app.services.NotificationFeedService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/NotificationFeed")
 public class NotificationFeedController {
+
+    private final NotificationFeedService notificationFeedService;
+
+    public NotificationFeedController(NotificationFeedService notificationFeedService) {
+        this.notificationFeedService = notificationFeedService;
+    }
 
     @GetMapping
     public List<Notification> getNotificationFeed(){
@@ -25,4 +30,5 @@ public class NotificationFeedController {
     /*public List<Notification> getNotificationFeedFromId(@PathVariable("id") String id){
         //filters url that filter through
     }*/
+
 }
