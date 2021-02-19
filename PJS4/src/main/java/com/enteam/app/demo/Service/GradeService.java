126383c2cc1eb24666578c02831cae86f9a7dad2
egsx <62139782+egsx@users.noreply.github.com>
2021-02-19 11:37:58 +0100

    Add files via upload

---
+++ PJS4/src/main/java/com/enteam/app/demo/Service/GradeService.java
@@ -0,0 +1,7 @@
+package Service;
+
+import com.enteam.app.demo.Grade;
+
+public interface GradeService {
+    public Grade addGrades();
+}
---
+++ PJS4/src/main/java/com/enteam/app/demo/Service/GradeServiceImplement.java
@@ -0,0 +1,12 @@
+package Service;
+
+import com.enteam.app.demo.Grade;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GradeServiceImplement implements GradeService {
+
+    public Grade addGrades() {
+        return null;
+    }
+}
